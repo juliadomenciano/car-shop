63a8338d90a1500d4779ae4ef2a0d59aaadf5636
Maju Domenciano <juliadomenciano@hotmail.com>
2022-09-14 21:03:35 -0300

    feat: Requisito 18
    IMotorcycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const categories = ['Street', 'Custom', 'Trail'] as const;
+
+const MotorcycleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+  category: z.enum(categories),
+  engineCapacity: z.number().max(2500),
+});
+
+type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+
+export { IMotorcycle, MotorcycleZodSchema };
